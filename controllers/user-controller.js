6c1e77547555f34e8f99f54030204a992043cae4
Austin Gilmore <austingilmora@gmail.com>
2022-02-10 19:21:19 -0500

    added update user route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -33,6 +33,17 @@ const userController = {
             console.log(err);
             res.status(500).json(err);
         });
+    },
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(500).json(err));
     }
 }
 
