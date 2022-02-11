95027b3363b1e9f47287519121d73b0ad1c11072
Austin Gilmore <austingilmora@gmail.com>
2022-02-10 21:09:16 -0500

    added add and delete friends routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -52,10 +52,39 @@ const userController = {
                     res.status(404).json({ message: 'No user found with this id!'})
                     return;
                 }
-                dbUserData.thoughts.remove();
                 res.json(dbUserData);
             })
             .catch(err => res.status(500).json(err));
+    },
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: params.friendId }},
+            { new: true, runValidators: true }
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(500).json(err));
+    },
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId }},
+            { new: true, runValidators: true }
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(500).json(err));
     }
 }
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const {
     getAllUsers,
     getUserById,
     updateUser,
-    deleteUser
+    deleteUser,
+    addFriend,
+    deleteFriend
 } = require('../../controllers/user-controller');
 
 router
@@ -17,5 +19,10 @@ router
     .get(getUserById)
     .put(updateUser)
     .delete(deleteUser)
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .put(deleteFriend)
 
 module.exports = router;
